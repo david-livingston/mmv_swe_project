52da0665e674b226badef517f260435ac005f513
David Livingston <dlivi2@uis.edu>
2011-03-13 18:04:12 -0400

    v 0.2 (update 3/13, added help feature -- needs content)

--- src/MenuBar.java
+++ src/MenuBar.java
@@ -17,7 +17,15 @@ import java.io.File;
     
 public class MenuBar extends JMenuBar implements ActionListener {
 
+    // TODO: installer, store these pages locally
+    final static String URL_HELP_TOPICS = "http://www.davidlivingston.info/mmv/help-topics.html";
+    final static String URL_ABOUT = "http://www.davidlivingston.info/mmv/about.html";
+    final static String URL_PROJECT_HOMEPAGE = "https://github.com/david-livingston/mmv_swe_project";
+
     final static String MENU_ITEM_KEY_SAVE_AS = "Save Image As...";
+    final static String MENU_ITEM_HELP_TOPICS = "Help Topics";
+    final static String MENU_ITEM_PROJECT_PAGE = "Project Homepage @ GitHub";
+    final static String MENU_ITEM_ABOUT = "About";
 
     final MandelJPanel mJPanel;
 
@@ -38,35 +46,75 @@ public class MenuBar extends JMenuBar implements ActionListener {
         add(new JMenu("Resolution"));
         add(new JMenu("Advanced Options"));
 
+        JMenu helpMenu = new JMenu("Help");
+        JMenuItem helpTopics = new JMenuItem(MENU_ITEM_HELP_TOPICS);
+        helpTopics.addActionListener(this);
+        helpMenu.add(helpTopics);
+        JMenuItem projectInfo = new JMenuItem(MENU_ITEM_PROJECT_PAGE);
+        projectInfo.addActionListener(this);
+        helpMenu.add(projectInfo);
+        JMenuItem about = new JMenuItem(MENU_ITEM_ABOUT);
+        about.addActionListener(this);
+        helpMenu.add(about);
+        add(helpMenu);
     }
 
     public void actionPerformed(ActionEvent e){
-        System.out.println("Action! " + e);
 
         // DO FILE SAVE ON IMAGE IF THAT WAS THE MENU ITEM SELECTION
-        if(e.getActionCommand().equals(MENU_ITEM_KEY_SAVE_AS)){
-            try {
-                // figure out where to save the image
-                // TODO: set file filters
-                // TODO: make dialog open in My Pictures
-                final JFileChooser jfc = new JFileChooser();
-                jfc.setSelectedFile(new File("mandel.png"));
-                final int retVal = jfc.showSaveDialog(mJPanel);
-
-                if(JFileChooser.APPROVE_OPTION != retVal){
-                    return; // user cancelled
-                }
-
-                File savefile = jfc.getSelectedFile();
-
-                // save image as PNG, doc re. saving BufferedImage:
-                // http://download.oracle.com/javase/tutorial/2d/images/saveimage.html
-                // TODO: other formats, esp. lossless format (bmp ?)
-                BufferedImage bi = mJPanel.getCurrentImage();
-                ImageIO.write(bi, "png", savefile);
-            } catch (Exception ioe) {
-                System.err.println(ioe);
-            }
+        if(matches(e, MENU_ITEM_KEY_SAVE_AS)){
+            saveImage("mandel", "png");
+        } else if (matches(e, MENU_ITEM_HELP_TOPICS)) {
+            openWebPage(URL_HELP_TOPICS);
+        } else if (matches(e, MENU_ITEM_ABOUT)) {
+            openWebPage(URL_ABOUT);
+        } else if (matches(e, MENU_ITEM_PROJECT_PAGE)) {
+            openWebPage(URL_PROJECT_HOMEPAGE);
+        } else {
+            System.err.println("Feature not implemented: " + e.getActionCommand());
         }
+    }
+
+    public boolean matches(ActionEvent e, String key){
+        return e.getActionCommand().equals(key);
+    }
+
+    public boolean saveImage(String defaultName, String defaultExtension){
+        try {
+            // figure out where to save the image
+            // TODO: set file filters
+            // TODO: make dialog open in My Pictures
+            final JFileChooser jfc = new JFileChooser();
+            jfc.setSelectedFile(new File(defaultName + "." + defaultExtension));
+            final int retVal = jfc.showSaveDialog(mJPanel);
+
+            if(JFileChooser.APPROVE_OPTION != retVal){
+                return false; // user cancelled
+            }
+
+            File savefile = jfc.getSelectedFile();
+
+            // save image as PNG, doc re. saving BufferedImage:
+            // http://download.oracle.com/javase/tutorial/2d/images/saveimage.html
+            // TODO: other formats, esp. lossless format (bmp ?)
+            BufferedImage bi = mJPanel.getCurrentImage();
+            ImageIO.write(bi, defaultExtension, savefile);
+        } catch (Exception ioe) {
+            System.err.println(ioe);
+            return false;
+        }
+        return true;
+    }
+
+    // this requires java 6, more complicated in java 5
+    // see: http://www.centerkey.com/java/browser/
+    public boolean openWebPage(String url){
+        try {
+            java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+        } catch (Exception e){
+            System.err.println("Error opening webpage: " + url + "\n\tError: " + e);
+            return false;
+        }
+        return true;
     }
 }
